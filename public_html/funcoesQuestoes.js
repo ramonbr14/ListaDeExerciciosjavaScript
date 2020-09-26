c3f65d4c05720e27a246cd0820aefe5697d0578b
Ramon dos Santos Silva <33532328+ramonbr14@users.noreply.github.com>
2020-09-26 12:35:37 -0400

    correção primos
    Ultima tentativa de corrigir a questão com primos antes de mandar para o professor

--- public_html/funcoesQuestoes.js
+++ public_html/funcoesQuestoes.js
@@ -4,6 +4,10 @@
  * and open the template in the editor.
  */
 
+//voltar
+function voltarIndex(){
+    document.location.href = "index.html";
+}
 
 // Questao 01
 function mostrarCaixa(){
@@ -80,8 +84,140 @@ function imc(){
         msn += " Alerta: OBESIDADE GRAU III - MORBIDA";
     }
     document.getElementById('nesse').innerText = msn; //usar quando for o id
+
     //document.querySelector('#nesse').innerText = msn; //usar quando tiver o class 
     
 }
 
 
+//questao 07
+
+function multiplo3e5(){
+    let soma = 0;
+    let msn = "";
+    for (let i = 1; i <= 1000; i++){
+        if(i%3==0||i%5==0){
+            soma = i+soma;
+        }
+    }
+    msn = "A Soma dos multiplos de 3 e 5 é: " + soma;
+    document.getElementById('aqui').innerText = msn;
+
+}
+
+//questão 08
+
+function fibonatisopares(){
+    let soma = 0;
+    let fib1 = 0;
+    let fib2 = 1;
+    let fibo = 0;
+    let msn = "";
+    
+    while(fibo <= 50000){
+        fibo = fib1 + fib2;
+    if(fibo%2==0){
+        soma = soma + fibo;
+    }
+    fib1 = fib2;
+    fib2 = fibo;
+    console.log("FIBO no valor: " + fibo);
+    }
+    msn = "A Soma dos termos pares da sequencia FIBONACCi é: " + soma;
+    document.getElementById('aqui').innerText = msn;
+        
+
+}
+
+//questão 08
+
+function numeroprimo(){
+    // Baseado nas regras do site https://matematicasimples.pmvs.pt/2018/01/27/calcular-numeros-primos/#:~:text=Assim%2C%20um%20n%C3%BAmero%20ser%C3%A1%20primo%20se%20tiver%20apenas,11%7D.%20O%20conjunto%20dos%20n%C3%BAmeros%20primos%20%C3%A9%20infinito.
+    let msn = "";
+    let primos = [ 2, 3, 5, 7, 11, 13];
+    let imprimos = "sequencia de primos: ";
+    let num = 13;
+    let somad = 0;
+    let d1 = 0;
+    let psp = 3;
+    let p = 0;
+    let pmx = 20;
+    let cont = 6;
+    let status = false;
+    //primos do exercicio 2 3 5 7 11 13
+    
+    //primos depois 17 19 23 31 37
+    
+    
+    //Regra: Primo Não pode ser um numero par
+    while(cont<pmx+1){
+        num = num +1;
+        console.log("Suposto primo: "+ num);
+        if(num%2 != 0){ //Regra do par
+            //console.log("REGRA DE 2");
+            //Proxima regra: Sona os digitos não poder ser divisivel por 3
+            d1 = num; 
+            somad = 0;
+            while (d1 > 0) {
+                somad   += d1 % 10;
+                d1  = parseInt(d1 / 10); // <--- atenção
+            }
+            
+            if(somad%3!=0){ //regra do primo 3
+            //console.log("REGRA DE 3");
+                // Não pode ser divisivel por 5
+                if(num%5!=0){ //regra do Primo 5
+                    //console.log("REGRA DE 5");
+                    //divisão pelo primo p testando os primos ate quociente ser menor que que o numero P e/ou o resto for diferente de 0
+                    
+                    do{
+                        if(num%p!=0){
+                            p = primos[psp]
+                            psp = psp +1;
+                            d1 = Math.round(num/p); // controle
+                            console.log("O resultado de " +num+ " dividido por "+p+" deve ser igual a: "+ d1); 
+                            somad = num%p; //controle 
+                            console.log("O RESTO" +num+ " dividido por "+p+" é "+ somad); 
+                                if(Math.round(num/p)>p && num%p!=0){
+                                    console.log("PASSOU PELOS TESTES NOS PRIMOS: "+num);
+                                    primos.push(num);
+                                    cont = cont+1;
+                                    status=true;
+                                    console.log("Status do teste PRIMO: "+status);
+                                    //console.log(cont);
+                                //console.log("TESTE FINAL: "+ num);
+                                }else{
+                                    status = false;
+                                    console.log("Status do teste PRIMO: "+status);
+                                }
+                        }else{
+                            status = true;
+                            console.log("Status do teste: "+status);
+                        }
+                    }while(status==false)
+                    // continua dando tudo errado!
+                        
+                    
+                }        
+            }
+        }
+    }
+    
+
+    d1=1;
+    for(let i = 0; i< primos.length; i++){
+        console.log("Primo "+d1+" é "+primos[i]);
+        imprimos += primos[i] + ", ";
+        d1++;
+    }
+    document.getElementById("primos").innerText = imprimos;
+
+
+    msn = "o 10º termo primo é: " + primos[pmx];
+    document.getElementById('aqui').innerText = msn;
+
+} 
+
+
+
+    
